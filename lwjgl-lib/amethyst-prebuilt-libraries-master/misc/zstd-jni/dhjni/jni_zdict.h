c5fbb24e10e4998c68a4caa8f267613b8e68fca7
T1k-T1k <razdobudko13@gmail.com>
2026-08-01 19:00:56 +0300

    fix: reset git index and force ignore lwjgl-lib

--- lwjgl-lib/amethyst-prebuilt-libraries-master/misc/zstd-jni/dhjni/jni_zdict.h
+++
@@ -1,5 +0,0 @@
-#include <jni.h>
-
-
-JNIEXPORT jlong Java_com_github_luben_zstd_Zstd_trainFromBufferDirect0 (JNIEnv *env, jclass obj, jobject samples, jintArray sampleSizes, jobject dictBuffer, jboolean legacy, jint compressionLevel);
-JNIEXPORT jlong Java_com_github_luben_zstd_Zstd_trainFromBuffer0 (JNIEnv *env, jclass obj, jobjectArray samples, jbyteArray dictBuffer, jboolean legacy, jint compressionLevel);
